5836a13ff5a6ca619f70b312244599e9d57a5dce
Nuha <nuhasafia123@gmail.com>
2023-08-01 10:44:43 -0700

    Initial Commit

---
+++ NoCoreDataToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoCoreDataToDo
+//
+//  Created by Nuha Mohamed on 8/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoCoreDataToDo/NoCoreDataToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoCoreDataToDoApp.swift
+//  NoCoreDataToDo
+//
+//  Created by Nuha Mohamed on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct NoCoreDataToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
